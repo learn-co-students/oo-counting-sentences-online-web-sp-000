e7d67db2b5d56ab3a294d94c1e7cc2e2a74b8db4
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-27 08:50:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,3 +27,14 @@ class String
     self.scan(/[^\.!?]+[\.!?]/).map.count
   end
 end
+
+#[^\.!?]:Caret inside of a character class [^ ] is the negation operator. Which means we are
+#looking for characters which are not present in list: ., ! and ?.
+
+# + :is a greedy operator that returns matches between 1 and unlimited times.
+#(capturing our sentences here and ignoring repetitions like ...)
+
+#matching characters ., ! or ?.
+
+# [\.!?] : In a nutshell, we are capturing all characters that are
+#not ., ! or ? till we get characters that are ., ! or ?. Which basically can be treated as a sentence (in broad senses).

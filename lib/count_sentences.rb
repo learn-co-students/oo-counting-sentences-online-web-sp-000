e654e073ecfe8601a2417b05b311ec62fe454393
Jon Mackey <jon@jonmackey.party>
2018-12-03 21:37:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")? true : false
   end
-
+    
   def question?
-
+    self.end_with?("?")? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!")? true : false
   end
 
   def count_sentences
-
+    self_array = [ ]
+    total_count = [ ]
+    self_array = self.split((/[.!'?]/))
+    self_array.delete_if{|x| x == ""}
+    total_count << self_array.count
+    total_count.inject {|sum, n| sum + n}
   end
 end

b39b7374b9f76d99a404a45a553ce6721aacfe88
Alexander Baker <alex.baker685@gmail.com>
2019-01-03 17:40:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,36 +4,40 @@ class String
 
   def sentence?
   sentence_array = self.split
-    if sentence_array[-1][-1] == "."
-      true
-    else
-      false
-    end
+    # if sentence_array[-1][-1] == "."
+    #   true
+    # else
+    #   false
+    # end
+    self.end_with?(".")
 
   end
 
   def question?
-    sentence_array = self.split
-    if sentence_array[-1][-1] == "?"
-      true
-    else
-      false
-    end
+    # sentence_array = self.split
+    # if sentence_array[-1][-1] == "?"
+    #   true
+    # else
+    #   false
+    # end
+    self.end_with?("?")
 
   end
 
   def exclamation?
-    sentence_array = self.split
-    if sentence_array[-1][-1] == "!"
-      true
-    else
-      false
-    end
+    # sentence_array = self.split
+    # if sentence_array[-1][-1] == "!"
+    #   true
+    # else
+    #   false
+    # end
+    self.end_with?("!")
   end
 
 def count_sentences
-    sentence_array = self.split(/[.?!]/)
-    sentence_array.reject! { |c| c.empty? }
-    sentence_array.count
+    # sentence_array = self.split(/[.?!]/)
+    # sentence_array.reject! { |c| c.empty? }
+    # sentence_array.count
+     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end

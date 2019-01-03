8333de6c910876646fb409d71b5334f8d8c8133e
Alexander Baker <alex.baker685@gmail.com>
2019-01-03 17:37:51 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+  sentence_array = self.split
+    if sentence_array[-1][-1] == "."
+      true
+    else
+      false
+    end
+
   end
 
   def question?
+    sentence_array = self.split
+    if sentence_array[-1][-1] == "?"
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    sentence_array = self.split
+    if sentence_array[-1][-1] == "!"
+      true
+    else
+      false
+    end
   end
 
-  def count_sentences
-
+def count_sentences
+    sentence_array = self.split(/[.?!]/)
+    sentence_array.reject! { |c| c.empty? }
+    sentence_array.count
   end
 end

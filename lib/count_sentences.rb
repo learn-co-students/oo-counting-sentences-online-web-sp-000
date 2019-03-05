06b5c2bb105802d3aa1c11244475f9907c1507e9
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-05 04:46:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
 class String
-
-  def sentence?
-    
+   def sentence?
+      self.end_with?(".")
   end
 
   def question?
-
+     self.end_with?("?")
   end
 
   def exclamation?
-
+     self.end_with?("!")
   end
 
   def count_sentences
+     self.split(/[.?!]/).reject { |c| c.empty? }.count
 
   end
 end

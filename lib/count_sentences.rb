3f4f234f63b991fa38305f1da9bb166e1bd59a10
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-09 02:32:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      false
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      return false
+    end 
   end
 
   def count_sentences
-
-  end
+    sentence_array = self.split(/[.?!]/)
+    count = sentence_array.length 
+    sentence_array.each do |sentence|
+      if sentence == ""
+        count -= 1 
+      end 
+    end
+    return count 
+  end 
 end

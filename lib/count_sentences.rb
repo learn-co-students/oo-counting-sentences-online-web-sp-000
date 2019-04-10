8f96c2592e9e702adb82f79d7d14be753cf80940
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 01:01:17 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    tube = self.split(/[\s,]/)
+    count = 0
+    tube.each do |x|
+      count += 1 if x.question?||x.question?||x.exclamation?||x.sentence?
+    end
+    return count
   end
 end

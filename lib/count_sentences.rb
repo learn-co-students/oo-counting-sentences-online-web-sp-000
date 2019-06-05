949948c12aa28cb502db50ac2e60321aaa4986a0
kevin lang <langkevina@gmail.com>
2019-06-05 00:46:03 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # "hello".include? "lo"   #=> true
+    self.include? "."
   end
 
   def question?
-
+    self.include? "?"
   end
 
   def exclamation?
-
+    self.include? "!"
   end
 
   def count_sentences
-
+    arr = self.split(/[.!?]/).reject{|s| s.empty?}
+    arr.size
   end
 end

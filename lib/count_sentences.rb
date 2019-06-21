3d706c35f6bc891e000f7e2c32643aa13123fde1
Frederic Silber <freddy.silber@cloudcoach.com>
2019-06-21 00:42:23 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).grep(/\S/).count
+    #sentences = []
+    #binding.pry
+    #single_punctuation = self.split("")
+    #single_punctuation.each do |char|
+    #  if char == "." || char == "?" || char == "!"
+    #    sentences << char
+    #  end
+    #end
+    #multiple_punctuation = self.split(" ")
+    #multiple_punctuation.each do |char|
+    #  if char == ".." || char == "??" || char == "!!" || char == "..." || char == "???" || char == "!!!" || char == "." || char == "!" || char == "?"
+    #    sentences << char
+    #  end
+    #end
+    #sentences.length
   end
 end

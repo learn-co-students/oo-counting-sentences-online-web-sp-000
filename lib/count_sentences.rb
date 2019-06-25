15529a6731d37c12b22551a4556c3920f2d3b0b9
Connor Cowan <ccowan003@comcast.net>
2019-06-25 21:29:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,24 +2,21 @@ require 'pry'
 
 class String
 
-  def sentence?(string)
-    if self.string.ends_With?(".")
-      return true
-    end
-  else
-    return false
-    end
+  def sentence?
+    self.end_with?(".")         #'self' in the body of this method to refer to the string on which the method is being called
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).delete_if {|string| string.size < 2}.size   #splits the string on any and all '. ! ?'
+                                                          #deletes every element of self for which block evaluates to true.
   end
+    #self.string.length
 end

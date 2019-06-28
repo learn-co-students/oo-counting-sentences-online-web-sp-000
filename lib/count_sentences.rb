e672d9e4ff4d445e12503f976ac6f52a8c057251
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-28 20:15:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
-
+require 'pry'
+# thing = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+delimiters = [". ", "! ", "? "]
   def count_sentences
-
+    delimiters = [". ", "! ", "? "]
+    self.split(Regexp.union(delimiters)).count
   end
+# binding.pry
 end

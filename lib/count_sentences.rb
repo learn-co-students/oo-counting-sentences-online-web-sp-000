e17fc1309dd79956d11f961bcfaa60e06c0e3730
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-01 13:00:17 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?(".") 
   end
 
   def question?
-
+ self.end_with?("?")
   end
 
   def exclamation?
-
+ self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = []
+new_array = self.squeeze.split(/[.!?]/)
+return new_array.length
   end
+  
 end

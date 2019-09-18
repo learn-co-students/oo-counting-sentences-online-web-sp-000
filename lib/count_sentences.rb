e3cb3d057e74f801144bb2c442072a659df5cc12
Haim D <haim11293@aol.com>
2019-09-18 08:50:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    # binding.pry
+    sentences_array = self.split(/\.|\?|\!/).reject{|string| string.empty?} # create a new array and split self, rejecting all empty spaces in the array.
+    return sentences_array.count # return the count of all the sentences in the split array.
   end
 end

cb10558e39c533096788de89f097f144f3e476ea
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-19 06:27:50 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    newArray = []
+    (self.split(" ")).collect do |i|
+      if i.end_with?("!") or i.end_with?("?") or i.end_with?(".")
+        newArray.push(i)
+      end
+    end
+    newArray.length
   end
+  
 end

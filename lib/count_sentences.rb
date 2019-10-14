4e6b182b18eed93922181b99ed6d1f9ddff5a8df
Anthony Tilelli <anthony@tilelli.me>
2019-10-14 21:18:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    count = 0
+    self.split.each do |x|
+      count += 1 if x[-1].match?(/[!\.?]/)
+    end
+    count
   end
 end

37c610005d80de10767a05cd957da373fb4e4c33
Doak Covington <doakcovington@protonmail.com>
2020-01-28 03:08:21 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.end_with?(".","?","!")
+      sentence_array = self.split(/[!?.]/)
+      complete_sentences = sentence_array.reject{|string| string.empty?}.count
+    else
+      return 0
+    end
+    complete_sentences
   end
 end

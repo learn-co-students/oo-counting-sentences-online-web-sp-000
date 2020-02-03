1036c36049c49035f2e62e4b7324ddfdfed5c137
Mark A Guza <guzamaguza@gmail.com>
2020-02-03 05:55:39 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+    delimiters = [".","?","!"]  #define delimiters
 
+    #take string that class is going to be called on
+    #split it up using the three delimiters
+    #reject any empty strings
+    #count numbers of items/sentences
+    self.split(Regexp.union(delimiters)).reject { |k| k.empty? }.count
   end
 end

2d077e42fcd2e793b8029737d2fe0ff8a8759429
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-09 19:08:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") 
+      return true 
+    else 
+      return false 
+    end
   end
 
   def question?
-
+    if self.end_with?("?") 
+      return true 
+    else  
+      return false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") 
+      return true 
+    else 
+      return false 
+    end
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
+  end
+  
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
   end
 end

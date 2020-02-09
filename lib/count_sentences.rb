f50a33a5814a10d1ad6c396cd26a9b6903eef009
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-02-09 19:09:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,10 +27,6 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).reject {|x| x.empty?}.size
-  end
-  
-  def count_sentences_words
-    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end

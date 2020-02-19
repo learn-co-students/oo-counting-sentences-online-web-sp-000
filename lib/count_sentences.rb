c300e3d53f4549672bc6999dc1f6e1d780726659
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-19 00:43:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    new = self.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s/)
+    new.length
   end
 end

9d9644689ff779d9d81412b9f1c69e27578b2a85
Alex Romo III <alexromo3rd@gmail.com>
2020-03-08 18:31:38 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
+# frozen_string_literal: true
+
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self[-1] == '.'
   end
 
   def question?
-
+    self[-1] == '?'
   end
 
   def exclamation?
-
+    self[-1] == '!'
   end
 
   def count_sentences
-
+    count = 0
+    split(/[.|!|?]/).each do |char|
+      count += 1 if char != ''
+    end
+    count
   end
 end

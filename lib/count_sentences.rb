b41e00d0634489f43c53062c9dd8e8e28f6f4b7b
Evan Burton <evan@heyevan.com>
2020-05-01 07:50:13 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
+  if self.end_with?(".")
+    true
+  else
+    false
   end
 end
+
+  def question?
+if self.end_with?("?")
+  true
+else
+  false
+  end
+end
+
+  def exclamation?
+if self.end_with?("!")
+  true
+else
+  false
+  end
+end
+
+def count_sentences
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
+  end
+end
+
+# def count_sentences
+# sentence_count = []
+# if exclamation? == true || question? == true || sentence? == true
+# self.squeeze.split(/[.?!]/).each do |sentence|
+# sentence_count<<sentence
+# end
+# sentence_count.length
+#       end
+#     end
+# end

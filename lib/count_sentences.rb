30963f38ec3a030ed09cffbfd4dbe370ec74fcfa
The-Gentleman <zraja4402@gmail.com>
2020-05-01 14:07:28 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.') ? true : false
   end
 
   def question?
-
+    self.end_with?('?') ? true : false
   end
 
   def exclamation?
-
+    self.end_with?('!') ? true : false
   end
 
   def count_sentences
-
+    delimiters = ['.','?','!']
+    array = self.split(Regexp.union(delimiters))
+    array.reject{|string| string.empty?}.count
   end
 end

5d1c208c4d09a248fbf233b5ea5c25a29a649506
Daniel Kim <daniel.kim52@gmail.com>
2020-05-01 08:08:50 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,14 @@ class String
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\!|\?/).delete_if {|w| w.size < 2}.size
   end
 end

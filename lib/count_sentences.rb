f4b407da0ef1f2afec9773efa63145b45dec2e43
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 22:24:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,20 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".")
+    unless self.end_with?(".") || self.end_with?(". ")
+      false
+    else
+      true
+    end
   end
 
   def question?
-    self.end_with?("?")
+    unless self.end_with?("?") || self.end_with?("? ")
+      false
+    else
+      true
+    end
   end
 
   def exclamation?
-    self.end_with?("!")
+    unless self.end_with?("!") || self.end_with?("! ")
+      false
+    else
+      true
+    end
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-    return new_array.length
+
+    self.split(/[.?!]+\s/).count
   end
 
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".split(/[.?!]+\s/)

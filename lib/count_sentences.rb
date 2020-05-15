c222e2ebcc6240ae6ac45dcfdb520bb647df12cc
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-15 19:27:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    if self.length > 1
+      self.gsub!(/[!?]/, ".").split(". ").count
+    else
+      0
+    end
   end
 end

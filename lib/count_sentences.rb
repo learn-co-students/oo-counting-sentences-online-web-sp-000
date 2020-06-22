c88bc5e7d056bacd3d734180f60af2d384f9fcc6
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-22 16:22:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    string = self.split(/[!+x?+x.+x]/)
+    string.delete("")
+    counted_string = string.count
+    counted_string
   end
 end

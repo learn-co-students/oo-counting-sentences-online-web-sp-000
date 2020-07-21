c709ec84303607789d87058298f4a5a26305154e
Ian Mayton <ian.mayton@gmail.com>
2020-07-21 21:16:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    counter = 0 
+    re = /[.!?]\s/
+    string_array = self.split(re)
+    return string_array.length
   end
+  
 end

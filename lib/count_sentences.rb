8262d34bd3dddad8ab5473f67d7c54aba96d6239
Nick Lanese <nlanese21@gmail.com>
2020-07-27 03:01:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,45 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    if !(self.include?(".") || self.include?("?") || self.include?("!"))
+      return 0
+    end
+    punct = 0
+    newSelf = self.split("")
+    i = 0
+    newSelf.each do | letter |
+      if (letter == "." || letter == "?" || letter == "!")
+        if !(newSelf[i + 1] == "." || newSelf[i + 1] == "?" || newSelf[i + 1] == "!")
+          punct += 1
+        end
+      end
+      i = i + 1
+    end
+    return punct
   end
+
 end

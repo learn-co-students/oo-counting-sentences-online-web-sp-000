a56d436e48088f85bdd9280e1bb88ffd36ab3212
Chandra Lama <iamme24cl@gmail.com>
2020-09-30 16:33:25 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
+  attr_accessor :sentence
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|word| word.length < 2}.length
   end
 end

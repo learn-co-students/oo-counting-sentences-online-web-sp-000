11534129eaf91fcf063d707c349630f773f89275
Kelly Hesterly <kelly.hesterly@gmail.com>
2020-10-02 19:25:02 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with? "."
+      true
+    else
+      self.end_with? "?" || "!"
+      false
+    end
   end
 
   def question?
-
+if self.end_with? "?"
+  true
+else
+  self.end_with? "." || "!"
+  false
   end
+end
 
   def exclamation?
-
+    if self.end_with? "!"
+      true
+    else
+      self.end_with? "." || "?"
+      false
+end
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
+
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+  end
+
 end

e3d0ced5a878eded5630c421c6c67b43300ca931
Alla Kuzminova <alchik@gmail.com>
2020-10-09 16:48:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+      self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    strings = self.split(/[.|?|!]/)
+    count = 0;
+    strings.each do |item|
+      if(item.size > 0)
+        count += 1
+      end
+    end
+    puts count
+    count
   end
 end

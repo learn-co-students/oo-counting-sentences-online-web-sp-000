55b9ea1f318be78728a1bc4d60f294aef6767525
fut0k0 <cccabell@icloud.com>
2020-10-14 15:35:32 -0700

    Passing lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
-require 'pry'
-
 class String
 
-  def sentence?
-    
-  end
+	def sentence?
+		self.end_with?(".")
+	end
 
-  def question?
+	def question?
+		self.end_with?("?")
+	end
 
-  end
+	def exclamation?
+		self.end_with?("!")
+	end
 
-  def exclamation?
+	def count_sentences
+		self.split(/[.?!]\s/).size
+	end
 
-  end
-
-  def count_sentences
-
-  end
 end

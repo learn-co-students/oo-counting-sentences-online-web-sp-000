0721a2537a7e6d1b43ffdf1b9d67723571122992
Andrew Eames <ajerxs@gmail.com>
2020-11-23 19:39:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    nu_array = []
+    self.split(/[.!?]/).each do |sentence|
+      nu_array << sentence
+    end
+    nu_array.delete_if {|element| element == ""}
+    nu_array.count
   end
+
 end

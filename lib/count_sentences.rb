5002e04b7da6c84dfdd5d7711392f506a2bbfc1e
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 17:43:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[\.|\?|\!]/).delete_if do |word|
+      word.length < 2
+    end.count
   end
 end

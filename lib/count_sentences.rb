8db279f7194a8aa0292dddc1b5fc985b4f4e6746
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-21 01:30:26 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if(self.match(/[.]$/) == nil)
+      return false
+    else
+      return true
+    end
   end
 
   def question?
-
+    if(self.match(/[?]$/) == nil)
+      return false
+    else
+      return true
+    end
   end
 
   def exclamation?
-
+    if(self.match(/[!]$/) == nil)
+      return false
+    else
+      return true
+    end
   end
 
   def count_sentences
-
+    arr = self.split(/[.?!]+/)
+    return (arr.length)
   end
 end

2d926407cffb704fbf5852cf9a683e900e481873
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-30 03:19:43 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -14,7 +14,16 @@ class String
     self.end_with?("!")
   end
 
-  def count_sentences 
+  def count_sentences
+    sentences = self.split("."|| "!"|| "?")
+    sentences.count 
     
+    #If an argument is given, counts the number of elements which equal obj using ==.
+    
+    
+    
+    if sentences.include? ""
+      puts sentences.length 
+    end  
   end
 end

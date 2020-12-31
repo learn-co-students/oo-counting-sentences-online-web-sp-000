c3109803d5542d4a326f9beb7d230b624f06f309
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-31 18:04:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -9,19 +9,17 @@ class String
   def question?
     self.end_with?("?")
   end
-
-  end
- c  def exclamation?
+ 
+   def exclamation?
     self.end_with?("!")
-omplex_String
-    complex_String = (self.split("."|| "!"|| "?")
-    #binding.pry 
-    complex_String.count 
-    
-   
-    
-    if (complex_String.include? "" 
-      puts complex_String.length 
-    end  
-  end
-end
+  end 
+  
+  
+  def count_sentences
+    complex_String = self.split("."|| "!"|| "?")
+    puts complex_String.count 
+     if (complex_String.include? "")
+      return complex_String.length 
+  end 
+end 
+end  

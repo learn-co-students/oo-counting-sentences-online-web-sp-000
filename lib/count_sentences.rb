eb49106773049aa24f3ea3caee4c8d3004b02efa
Ben Hayes <b.t.hayes@comcast.net>
2021-01-03 20:01:00 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentences = self.split(/[.?!]/)
+    if split_sentences.include?("")
+      split_sentences.delete("")
+    end
+    split_sentences.count
   end
 end
